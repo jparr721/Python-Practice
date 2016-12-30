5cef9275e7bb58a5278fdb6a5d4690569b920c6a
Jarred Parr <jparr721@gmail.com>
2016-12-30 15:21:50 -0500

    Removed redundant code, added comments. All done! Starting project 2

--- MSU CS Project 1/project1/proj01.py
+++ MSU CS Project 1/project1/proj01.py
@@ -1,4 +1,3 @@
-import sys
 
 """
 Python practice project using one of the Project Guidelines assigned
@@ -6,6 +5,7 @@ to the Michigan State University project list.
 
 Developed by: Jarred Parr
 
+Date created: 12/29/2016
 Version: 1.0
 """
 
@@ -33,46 +33,35 @@ class proj01:
         result = 0.6215*temp - (11.37*speed ** 0.16) + (0.3965*temp*speed ** 0.16) + 13.12
         return result
 
+    """
+    Display the welcome screen and run the calculations for the program
+    """
     def welcomeScreen(self):
-
-        choice = 0
         print "\n\n Welcome to the Wind Chill Calculator!"
-        print "\n1. Calculate wind chill for Farenheight and MPH"
+        print "\n1. Calculate wind chill for Fahrenheit and MPH"
         print "\n\n2. Calculate wind chill for Celsius and KMH"
 
         choice = int(input("\nPlease make a selection: "))
 
         if choice == 1:
+            #take input variables for temp and wind speed
             tempF = int(input("\nPlease enter your temperature(in F): "))
             speedMPH = int(input("\nPlease enter your wind speed(in MPH): "))
 
+            #print calculations
             print "\n\nThinking..."
             print "The wind chill temperature index (in F and MPH) is: "
             print round(calc.chillCalculatorF(tempF, speedMPH), 2)
 
         if choice == 2:
+            #take input variables for temp and wind speed
             tempC = int(input("\nPlease enter your temperature(in C): "))
             speedKPH = int(input("\nPlease enter your wind speed(in KPH): "))
 
+            #print calculations
             print "\n\nThinking..."
             print "The wind chill temperature index (in C and KPH) is: "
             print round(calc.chillCalculatorC(tempC, speedKPH), 2)
-
-    def programRestart(self):
-        print "\nWould you like to make another calculation?"
-        print "\n 1. Yes"
-        print "\n 2. No, exit the program"
-
-        choice = int(input("\n\n Please make a selection: "))
-
-        if choice == 1:
-            calc.welcomeScreen()
-            return True
-        if choice == 2:
-            return False
-            sys.exit()
-
-
 
 if __name__ == '__main__':
 
@@ -86,7 +75,6 @@ if __name__ == '__main__':
         print "----------------------------------------------------------------------------"
         print "----------------------------------------------------------------------------"
         calc.welcomeScreen()
-        calc.programRestart()
         print "\nRun Again? 1. Yes  2. No"
         choice = int(input("Enter your selection: "))
 
