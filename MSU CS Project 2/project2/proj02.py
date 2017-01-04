6d69877f1a1e51473acc50aab920d7d50be7ec9b
Jarred Parr <jparr721@gmail.com>
2017-01-04 15:37:08 -0500

    added Project 2 file, starting today

---
+++ MSU CS Project 2/project2/proj02.py
@@ -0,0 +1,11 @@
+"""
+MSU CSE 231 Project 2
+
+--This assignment focuses on the design, implementation and testing of a Python program to solve
+an arithmetic puzzle using repetition and selection.--
+
+Developed on: 12/30/16
+Developed by: Jarred Parr
+
+Version: 1.0
+"""
