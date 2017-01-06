540c37779edd18ae96d927780b09aac392b9657d
Jarred Parr <jparr721@gmail.com>
2017-01-06 13:16:15 -0500

    Almost done

--- MSU CS Project 2/project2/proj02.py
+++ MSU CS Project 2/project2/proj02.py
@@ -9,3 +9,22 @@ Developed by: Jarred Parr
 
 Version: 1.0
 """
+
+class proj02:
+
+    def addUntilComplete(self):
+
+        a = int(input("Input an integer: "))
+        while (a < 0):
+            a = int(input("ERROR, ENTER A POSITIVE NUMBER: "))
+
+        numberArr = [int(i) for i in str(a)]
+        total = sum([int(i) for i in str(a)])
+        output = list(n for n in numberArr + ' +')
+        return output + " = " + total
+
+if __name__ == '__main__':
+
+    p = proj02()
+
+    p.addUntilComplete()
