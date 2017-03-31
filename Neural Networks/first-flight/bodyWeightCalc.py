9b8536b1143be13a23de9476b6b884b24e63e402
Jarred Parr <jparr721@gmail.com>
2017-03-31 13:51:23 -0400

    Working on getting anaconda working right.

---
+++ Neural Networks/first-flight/bodyWeightCalc.py
@@ -0,0 +1,6 @@
+# This program checks bodyweight based on brain weight of an animal
+# This code will use a neural network to achieve this based on a data set
+
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
